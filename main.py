3041821199e59c6eefddace6943eba6b25852ae3
Guo Yu <tflsguoyu@gmail.com>
2021-11-01 10:21:55 -0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Instead of using sudo systemctl start docker use: sudo /etc/init.d/docker start , as of right now we do not have systemd in WSL 2.
-# sudo docker run --gpus all -it --rm -v `pwd`/:/gytest/ nvcr.io/nvidia/pytorch:21.08-py3
+# sudo docker run --gpus all -it --rm -v `pwd`/:/workspace/ nvcr.io/nvidia/pytorch:21.08-py3
+# use "$ exit" to disconnet docker and go back to WSL  
 import torch
 
 print(torch.cuda.is_available())
